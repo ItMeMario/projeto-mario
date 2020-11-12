24f3f5961dade1b0a6e88f818e28abe258f4a025
ItMeMario <matheust.melo@yahoo.com.br>
2020-11-12 20:43:31 -0300

    commit de emergência
    garantir que nenhuma alteração se perca por problemas de hardwere

--- src/routesCertificado.php
+++ src/routesCertificado.php
@@ -7,7 +7,7 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/certificado/[{sucesso}]', function (Request $request, Response $response, array $args) use ($container) {
+    $app->get('/certificado/[{id}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/certificado/' route");
 
@@ -16,6 +16,13 @@ return function (App $app) {
             exit;
         }
         $conexao = $container->get('pdo'); //conexão com o banco
+
+        $resultSet = $conexao->query('SELECT * FROM usuario_evento
+        WHERE usuario_id = "' . $_SESSION['login']['id'] . '"  ')->fetchAll();
+        $args['usuario_eventos'] = $resultSet;
+
+        $resultSet = $conexao->query('SELECT * FROM usuario_evento')->fetchAll();
+        $args['eventos_todos'] = $resultSet;
 
 
         // Render index view
